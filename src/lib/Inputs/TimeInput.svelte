fe8d9621a385c4b4d360fefd12b52baa2bfffbba
Noelle Stern <noellesternchen@gmail.com>
2026-05-03 19:56:07 +0500

    TimeInput disabled style

--- src/lib/Inputs/TimeInput.svelte
+++ src/lib/Inputs/TimeInput.svelte
@@ -25,15 +25,18 @@
 <div class="relative">
 
   <!-- Visible "fake" input -->
-  <div class="input input-bordered input-primary outline-primary disabled:border-primary disabled:opacity-30 p-0">
-    <div class="flex justify-center">
+  <button 
+    {disabled} 
+    class="input pointer-events-none p-0 disabled:border-base-content/15"
+  >
+    <div class="flex justify-center"> 
 
       <input
         {disabled}
         type="text"
         maxlength="2"
         inputmode="numeric"
-        class="w-[31px] text-right select-all"
+        class="pointer-events-auto w-[31px] text-right select-all"
         bind:this={hourElement}
       >
 
@@ -44,18 +47,19 @@
         type="text"
         maxlength="2"
         inputmode="numeric"
-        class="w-[31px] text-left select-all"
+        class="pointer-events-auto w-[31px] text-left select-all"
         bind:this={minuteElement}
       >
 
     </div>
-  </div>
+  </button>
 
   <!-- Hidden actual input used for mobile devices -->
   <input 
+    {disabled}
     type="time"
     bind:this={nativeElement}
-    class="absolute top-0 left-0 size-full { $mobile ? 'opacity-0' : 'hidden' }"
+    class="absolute top-0 left-0 size-full { ($mobile && !disabled) ? 'opacity-0' : 'hidden' }"
   >
 
 </div>
